18a85656bf41f4ef1b9034535a94f2d6d884ca42
MariaMckenzie <68311077+MariaMckenzie@users.noreply.github.com>
2022-11-18 14:06:17 -0500

    Create signup.data.js

---
+++ data/signup.data.js
@@ -0,0 +1,34 @@
+module.exports = [
+    {
+        firstname: "Tom",
+        lastname: "Smith",
+        email: "tomsmith@mail.com",
+        password: "P@ssw0rd",
+        confirmPassword: "P@ssw0rd",
+        isChecked: true,
+    },
+    {
+        firstname: "Tom",
+        lastname: "Smith",
+        email: "tomsmith@mail.com",
+        password: "P@ssw0rd",
+        confirmPassword: "P@ssw0rd",
+        isChecked: false,
+    },
+    {
+        firstname: "Tom",
+        lastname: "",
+        email: "",
+        password: "P@ssw0rd",
+        confirmPassword: "P@ssw0rd",
+        isChecked: false,
+    },
+    {
+        firstname: "",
+        lastname: "",
+        email: "",
+        password: "",
+        confirmPassword: "",
+        isChecked: false,
+    },
+]
