a6eb0f1bcfd2d217c41b44e6c5b669c20b79f889
MariaMckenzie <68311077+MariaMckenzie@users.noreply.github.com>
2022-11-19 18:11:35 -0500

    Create login.page.js

---
+++ test/pageobjects/login.page.js
@@ -0,0 +1,52 @@
+
+
+const Page = require('./page');
+
+/**
+ * sub page containing specific selectors and methods for a specific page
+ */
+class LoginPage extends Page {
+    /**
+     * define selectors using getter methods
+     */    
+    get inputEmail () {
+        return $('#email_address');
+    }
+
+    get inputPassword () {
+        return $('#password');
+    }
+
+    get btnSubmit () {
+        return $('#send2');
+    }
+
+    //ERROR MESSAGES    
+    get errorInputEmail () {
+        return $('#email-error');
+    }
+
+    get errorInputPassword () {
+        return $('#pass-error');
+    }
+   
+
+    /**
+     * a method to encapsule automation code to interact with the page
+     */
+    async login (email, password) {
+        await this.inputEmail.setValue(email);
+        await this.inputPassword.setValue(password);            
+        await this.btnSubmit.click();
+    }
+
+
+    /**
+     * overwrite specific options to adapt it to page object
+     */
+    open () {
+        return super.open('customer/account/login');
+    }
+}
+
+module.exports = new LoginPage();
