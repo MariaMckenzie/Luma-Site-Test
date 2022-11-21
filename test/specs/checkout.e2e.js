12771b5e165d57f87f07c9abc607e706a317c0d6
MariaMckenzie <68311077+MariaMckenzie@users.noreply.github.com>
2022-11-21 11:32:49 -0500

    fixed failing tests

--- test/specs/checkout.e2e.js
+++ test/specs/checkout.e2e.js
@@ -2,23 +2,20 @@ const productData = require("../data/product.data");
 const checkoutShippingPage = require("../pageobjects/checkoutShipping.page");
 
 describe('Luma Ecommerce Site - Go to Checkout', () => {
+    let itemName,  itemQuantity;
 
     it('should go to checkout from the dropdown cart', async () => {
         //variables that store product information
         [
-            itemType, itemCategory, 
-            itemSubcategory, itemName, 
-            itemSize, itemColour, 
-            itemQuantity, itemPrice
+            itemName,
+            itemQuantity
         ] = [
-            productData[4].type, productData[4].category,
-            productData[4].subcategory, productData[4].name,
-            productData[4].size, productData[4].colour,
-            productData[4].quanitity, productData[4].price
+            "Sprite Foam Roller", 
+            1
         ];
         
         //url for item
-        await browser.url('https://magento.softwaretestingboard.com/push-it-messenger-bag.html'); 
+        await browser.url('https://magento.softwaretestingboard.com/sprite-foam-roller.html'); 
         
         //select the quantity
         const quantity = await $('#qty');  
@@ -52,42 +49,53 @@ describe('Luma Ecommerce Site - Go to Checkout', () => {
     });  
 
     
-    it.skip('should go to checkout using the button from the view cart page', async () => {
-        //url for item
-        await browser.url('https://magento.softwaretestingboard.com/set-of-sprite-yoga-straps.html'); 
-
-        //we use default quantity (1)
+    it('should go to checkout using the button from the view cart page', async () => {        
+        //variables that store product information
+        [
+            itemName,
+            itemQuantity
+        ] = [
+            "Clamber Watch",
+            2
+        ];
         
-        //add item to cart
+        //url for item
+        await browser.url('https://magento.softwaretestingboard.com/clamber-watch.html'); 
+        
+        //select the quantity
+        const quantity = await $('#qty');  
+        await quantity.setValue(itemQuantity);
+
+        //add to cart
         const addToCartButton = await $('#product-addtocart-button'); 
         await expect(addToCartButton).toBeClickable(); 
         await expect(addToCartButton).toHaveText('Add to Cart'); 
         await addToCartButton.click();
 
-        //view cart
+        //wait for alert to confirm item has been added to cart
+        const alert = await $("div[data-bind='html: $parent.prepareMessageForHtml(message.text)']");        
+        await alert.waitForDisplayed(5000, true);
+        await expect(alert).toHaveTextContaining(`You added ${itemName} to your shopping cart.`);
+
         const cartIcon = await $('.action.showcart');
         await cartIcon.click();
 
         const cartDropdown = await $('#ui-id-1');
         await cartDropdown.waitForDisplayed(5000, true);
-
-        const viewCart = await $('.action.viewcart');
-        await viewCart.click();
-
+       
         //go to checkout
-        const goToCheckOutButton = await $("button[title='Proceed to Checkout'] span");
+        const goToCheckOutButton = await $("button[title='Proceed to Checkout']");
         await goToCheckOutButton.click();
 
         //check the heading on the page and title of browser
-        const pageHeading = await $('step-title');
-        await expect(pageHeading).toHaveText('Shipping Address');
+        await expect(checkoutShippingPage.pageHeading).toHaveText('Shipping Address');
         await expect(browser).toHaveTitleContaining('Checkout');
     });
 
     
-    it.skip('should go to checkout using direct link', async () => {
+    it('should go to checkout using direct link', async () => {
         //url for item
-        await browser.url('https://magento.softwaretestingboard.com/sprite-foam-roller.html'); 
+        await browser.url('https://magento.softwaretestingboard.com/savvy-shoulder-tote.html'); 
 
         //we use default quantity (1)
         
