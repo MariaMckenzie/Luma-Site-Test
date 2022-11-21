d34af98f7c05794b7f6b97f93d3cc623fbfef03b
MariaMckenzie <68311077+MariaMckenzie@users.noreply.github.com>
2022-11-21 11:06:40 -0500

    added tests for flows to go to checkout

--- test/specs/checkout.e2e.js
+++ test/specs/checkout.e2e.js
@@ -1,48 +1,70 @@
-const signupData = require("../data/signup.data");
+const productData = require("../data/product.data");
+const checkoutShippingPage = require("../pageobjects/checkoutShipping.page");
 
-describe.skip('Luma Ecommerce Site - Go to Checkout', () => {
-    let firstname, lastname, company, streetAddress1, streetAddress2, streetAddress3, city, state, zip, country, phone;
+describe('Luma Ecommerce Site - Go to Checkout', () => {
 
-    it('should not proceed if all required fields are not filled', async () => {
-        //variables that store address information
+    it('should go to checkout from the dropdown cart', async () => {
+        //variables that store product information
         [
-            firstname, lastname, company, 
-            streetAddress1, streetAddress2, streetAddress3, 
-            city, state, zip, 
-            country, phone
+            itemType, itemCategory, 
+            itemSubcategory, itemName, 
+            itemSize, itemColour, 
+            itemQuantity, itemPrice
         ] = [
-            signupData[6].firstname, signupData[6].lastname, '', 
-            'Apartment 24', '1 Merry Meadows Avenue', '',
-            'Miami', 'Florida', '000000-0000', 
-            'United States', '987654321'
+            productData[4].type, productData[4].category,
+            productData[4].subcategory, productData[4].name,
+            productData[4].size, productData[4].colour,
+            productData[4].quanitity, productData[4].price
         ];
-
-        //add item to cart
+        
         //url for item
-        await browser.url('https://magento.softwaretestingboard.com/didi-sport-watch.html'); 
-
+        await browser.url('https://magento.softwaretestingboard.com/push-it-messenger-bag.html'); 
+        
         //select the quantity
         const quantity = await $('#qty');  
-        await quantity.setValue(1);
-        
+        await quantity.setValue(itemQuantity);
+
+        //add to cart
         const addToCartButton = await $('#product-addtocart-button'); 
         await expect(addToCartButton).toBeClickable(); 
         await expect(addToCartButton).toHaveText('Add to Cart'); 
         await addToCartButton.click();
 
-        //go to checkout - using button in dropdown cart
+        //wait for alert to confirm item has been added to cart
+        const alert = await $("div[data-bind='html: $parent.prepareMessageForHtml(message.text)']");        
+        await alert.waitForDisplayed(5000, true);
+        await expect(alert).toHaveTextContaining(`You added ${itemName} to your shopping cart.`);
+
+        const cartIcon = await $('.action.showcart');
         await cartIcon.click();
 
+        const cartDropdown = await $('#ui-id-1');
         await cartDropdown.waitForDisplayed(5000, true);
-
-        const goToCheckOutButton = await $('#top-cart-btn-checkout');
+       
+        //go to checkout using the checkout button in dropdown
+        const goToCheckOutButton = await $('button#top-cart-btn-checkout');
+        await goToCheckOutButton.waitForDisplayed(5000, true);
         await goToCheckOutButton.click();
 
-
         //check the heading on the page and title of browser
-        await expect(pageHeading).toHaveText('Shipping Address');
+        await expect(checkoutShippingPage.pageHeading).toHaveText('Shipping Address');
         await expect(browser).toHaveTitleContaining('Checkout');
+    });  
 
+    
+    it.skip('should go to checkout using the button from the view cart page', async () => {
+        //url for item
+        await browser.url('https://magento.softwaretestingboard.com/set-of-sprite-yoga-straps.html'); 
+
+        //we use default quantity (1)
+        
+        //add item to cart
+        const addToCartButton = await $('#product-addtocart-button'); 
+        await expect(addToCartButton).toBeClickable(); 
+        await expect(addToCartButton).toHaveText('Add to Cart'); 
+        await addToCartButton.click();
+
+        //view cart
         const cartIcon = await $('.action.showcart');
         await cartIcon.click();
 
@@ -52,28 +74,35 @@ describe.skip('Luma Ecommerce Site - Go to Checkout', () => {
         const viewCart = await $('.action.viewcart');
         await viewCart.click();
 
-        //check for clothing in the cart and assert that all the information is correct
-        const firstCartItemName = await $('body > div:nth-child(5) > main:nth-child(3) > div:nth-child(4) > div:nth-child(1) > div:nth-child(5) > form:nth-child(2) > div:nth-child(2) > table:nth-child(1) > tbody:nth-child(3) > tr:nth-child(1) > td:nth-child(1) > div:nth-child(2) > strong:nth-child(1) > a:nth-child(1)');
-        await expect(firstCartItemName).toHaveText(itemName);
+        //go to checkout
+        const goToCheckOutButton = await $("button[title='Proceed to Checkout'] span");
+        await goToCheckOutButton.click();
+
+        //check the heading on the page and title of browser
+        const pageHeading = await $('step-title');
+        await expect(pageHeading).toHaveText('Shipping Address');
+        await expect(browser).toHaveTitleContaining('Checkout');
+    });
+
+    
+    it.skip('should go to checkout using direct link', async () => {
+        //url for item
+        await browser.url('https://magento.softwaretestingboard.com/sprite-foam-roller.html'); 
+
+        //we use default quantity (1)
         
-        const firstCartItemSize = await $('body > div:nth-child(5) > main:nth-child(3) > div:nth-child(4) > div:nth-child(1) > div:nth-child(5) > form:nth-child(2) > div:nth-child(2) > table:nth-child(1) > tbody:nth-child(3) > tr:nth-child(1) > td:nth-child(1) > div:nth-child(2) > dl:nth-child(2) > dd:nth-child(2)');
-        await expect(firstCartItemSize).toHaveText(itemSize.toString());
+        //add item to cart
+        const addToCartButton = await $('#product-addtocart-button'); 
+        await expect(addToCartButton).toBeClickable(); 
+        await expect(addToCartButton).toHaveText('Add to Cart'); 
+        await addToCartButton.click();
 
-        const firstCartItemColor = await $('body > div:nth-child(5) > main:nth-child(3) > div:nth-child(4) > div:nth-child(1) > div:nth-child(5) > form:nth-child(2) > div:nth-child(2) > table:nth-child(1) > tbody:nth-child(3) > tr:nth-child(1) > td:nth-child(1) > div:nth-child(2) > dl:nth-child(2) > dd:nth-child(4)');
-        await expect(firstCartItemColor).toHaveText(itemColour);
+        //go to checkout using direct link
+        await checkoutShippingPage.open();
 
-        const firstCartItemQuantity = await $('body > div:nth-child(5) > main:nth-child(3) > div:nth-child(4) > div:nth-child(1) > div:nth-child(5) > form:nth-child(2) > div:nth-child(2) > table:nth-child(1) > tbody:nth-child(3) > tr:nth-child(1) > td:nth-child(3) > div:first-child > div:first-child > label:first-child > input');
-        await expect(firstCartItemQuantity).toHaveAttrContaining('value', itemQuantity);
+        //check the heading on the page and title of browser
+        await expect(checkoutShippingPage.pageHeading).toHaveText('Shipping Address');
+        await expect(browser).toHaveTitleContaining('Checkout');
+    });
 
-        const firstCartItemPrice = await $('body > div:nth-child(5) > main:nth-child(3) > div:nth-child(4) > div:nth-child(1) > div:nth-child(5) > form:nth-child(2) > div:nth-child(2) > table:nth-child(1) > tbody:nth-child(3) > tr:nth-child(1) > td:nth-child(2) > span:nth-child(1) > span:nth-child(1) > span:nth-child(1)');
-        await expect(firstCartItemPrice).toHaveText(`$${itemPrice.toFixed(2)}`);
-
-        const firstCartItemSubtotal = await $('body > div:nth-child(5) > main:nth-child(3) > div:nth-child(4) > div:nth-child(1) > div:nth-child(5) > form:nth-child(2) > div:nth-child(2) > table:nth-child(1) > tbody:nth-child(3) > tr:nth-child(1) > td:nth-child(4) > span:nth-child(1) > span:nth-child(1) > span:nth-child(1)');
-        await expect(firstCartItemSubtotal).toHaveText(`$${(itemPrice * itemQuantity).toFixed(2)}`);
-    });  
-
-
-
-//purchasing a product
-//verifying purchase in order history 
 });
