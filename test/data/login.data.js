3f8a4e94f47021be70af79ece5d435e00c8b85b8
MariaMckenzie <68311077+MariaMckenzie@users.noreply.github.com>
2022-11-21 11:43:43 -0500

    added login data

---
+++ test/data/login.data.js
@@ -0,0 +1,26 @@
+module.exports = [
+    { //0
+        firstname: "Tom",
+        lastname: "Smith",
+        email: "tomsmith@serendipity.com",
+        password: "P@ssw0rd"
+    },
+    { //1
+        firstname: "Tina",
+        lastname: "Smith",
+        email: "tinasmith@serendipity.com",
+        password: "P@ssw0rd"
+    },
+    { //3
+        firstname: "Tom",
+        lastname: "Smith",
+        email: "tomsmith@mail.com",
+        password: "P@ssw0rd"
+    },
+    { //4
+        firstname: "Jane",
+        lastname: "Doe",
+        email: "janedoe@mail.com",
+        password: "P@ssw0rd"
+    },
+]
